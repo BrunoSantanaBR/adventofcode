354c4f8654f9832cd76fc4fed650b7cd2fcc02c2
BrunoSantanaBR <bruno.raphael.marques.santana@gmail.com>
2022-07-03 21:46:08 +0200

    Day2 - Part 1

---
+++ Day2/src/main/java/adventofcode2021/Day2.java
@@ -0,0 +1,46 @@
+package adventofcode2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Day2 {
+
+	private static final String RESOURCE_FILE = "src/main/resources/puzzleInput.txt";
+
+	public static void main(String[] args) throws IOException {
+
+		List<String> movementsArray = getPuzzleInput();
+		System.out.println(getFinalHorizontalDepthPosition(movementsArray));
+		System.out.println("Finish");
+	}
+
+	private static int getFinalHorizontalDepthPosition(List<String> movementsArray) {
+
+		int horizontal = 0;
+		int depth = 0;
+
+		for (String movement : movementsArray) {
+
+			if (movement.indexOf("forward") > -1) {
+				horizontal = horizontal + Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
+			} else if (movement.indexOf("down") > -1) {
+				depth = depth + Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
+			}else if (movement.indexOf("up") > -1) {
+				depth = depth - Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
+			}
+
+		}
+		
+		
+		return horizontal * depth;
+	}
+
+	private static List<String> getPuzzleInput() throws IOException {
+		return Files.readAllLines(Paths.get(RESOURCE_FILE)).stream().map(String::trim).collect(Collectors.toList());
+	}
+}
