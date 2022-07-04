8f81babe5261f2c7e9123cafeadbaa64176cd4ec
tvs <teije-vs@hotmail.com>
2022-07-04 22:15:16 +0200

    day2

--- Day2/src/main/java/adventofcode2021/Day2.java
+++ Day2/src/main/java/adventofcode2021/Day2.java
@@ -4,68 +4,91 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Day2 {
+  private static final String RESOURCE_FILE = "src/main/resources/puzzleInput.txt";
 
-	private static final String RESOURCE_FILE = "src/main/resources/puzzleInput.txt";
+  public static void main(String[] args) throws IOException {
+    List<Instruction> movementsArray = getPuzzleInput();
+    System.out.println(getFinalHorizontalDepthPosition(movementsArray));
+    System.out.println(getFinalHorizontalDepthPositionWithAim(movementsArray));
+    System.out.println("Finish");
+  }
 
-	public static void main(String[] args) throws IOException {
+  private static int getFinalHorizontalDepthPositionWithAim(List<Instruction> movementsArray) {
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
 
-		List<String> movementsArray = getPuzzleInput();
-		System.out.println(getFinalHorizontalDepthPosition(movementsArray));
-		System.out.println(getFinalHorizontalDepthPositionWithAim(movementsArray));
-		System.out.println("Finish");
-	}
+    for (var instruction : movementsArray) {
+      if (instruction.isForward()) {
+        horizontal = horizontal + instruction.getAmount();
+        depth = depth + (instruction.getAmount() * aim);
+      }
+      if (instruction.isDown()) {
+        aim = aim + instruction.getAmount();
+      }
+      if (instruction.isUp()) {
+        aim = aim - instruction.getAmount();
+      }
+    }
+    return horizontal * depth;
+  }
 
-	private static int getFinalHorizontalDepthPositionWithAim(List<String> movementsArray) {
+  private static int getFinalHorizontalDepthPosition(List<Instruction> movementsArray) {
+    int horizontal = 0;
+    int depth = 0;
 
-		int horizontal = 0;
-		int depth = 0;
-		int aim = 0;
+    for (var instruction : movementsArray) {
+      if (instruction.isForward()) {
+        horizontal = horizontal + instruction.getAmount();
+      }
+      if (instruction.isDown()) {
+        depth = depth + instruction.getAmount();
+      }
+      if (instruction.isUp()) {
+        depth = depth - instruction.getAmount();
+      }
+    }
+    return horizontal * depth;
+  }
 
-		for (String movement : movementsArray) {
+  private static List<Instruction> getPuzzleInput() throws IOException {
+    return Files.readAllLines(Paths.get(RESOURCE_FILE)).stream()
+        .map(String::trim)
+        .map(s -> s.split(" "))
+        .map(Instruction::new)
+        .collect(toList());
+  }
+}
 
-			if (movement.indexOf("forward") > -1) {
-				int unit = Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
-				horizontal = horizontal + unit;
-				depth = depth + (unit * aim);
-			} else if (movement.indexOf("down") > -1) {
-				aim = aim  + Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
-			}else if (movement.indexOf("up") > -1) {
-				aim = aim - Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
-			}
+class Instruction {
+  private static final String FORWARD = "forward";
+  private static final String DOWN = "down";
+  private static final String UP = "up";
+  private final String direction;
+  private final int amount;
 
-		}
-		
-		
-		return horizontal * depth;
-	}
+  public Instruction(String[] line) {
+    this.direction = line[0];
+    this.amount = Integer.parseInt(line[1]);
+  }
 
-	private static int getFinalHorizontalDepthPosition(List<String> movementsArray) {
+  public boolean isForward() {
+    return direction.contains(FORWARD);
+  }
 
-		int horizontal = 0;
-		int depth = 0;
+  public boolean isDown() {
+    return direction.contains(DOWN);
+  }
 
-		for (String movement : movementsArray) {
+  public boolean isUp() {
+    return direction.contains(UP);
+  }
 
-			if (movement.indexOf("forward") > -1) {
-				horizontal = horizontal + Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
-			} else if (movement.indexOf("down") > -1) {
-				depth = depth + Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
-			}else if (movement.indexOf("up") > -1) {
-				depth = depth - Integer.parseInt(movement.substring(movement.indexOf(" ") + 1));
-			}
-
-		}
-		
-		
-		return horizontal * depth;
-	}
-
-	private static List<String> getPuzzleInput() throws IOException {
-		return Files.readAllLines(Paths.get(RESOURCE_FILE)).stream().map(String::trim).collect(Collectors.toList());
-	}
+  public int getAmount() {
+    return amount;
+  }
 }
